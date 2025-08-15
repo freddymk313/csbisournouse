79b858ee8be19dadc6f58f1f4798516d632a4c32
freddymk313 <107887953+freddymk313@users.noreply.github.com>
2025-08-15 14:27:48 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import Header from './components/Header.tsx'
 import Footer from './components/Footer.tsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <App />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
